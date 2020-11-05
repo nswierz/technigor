af1570637ec73514c708e61c54bc1159148596e6
Natalia <nswierz.iwp@gmail.com>
2020-11-05 19:34:10 +0100

    mój pierwszy commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { Index } from './components/index.component'
+const routes: Routes = [{
+  path:'dupa', component:Index
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/index.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component ({
+  templateUrl: './index.component.html'
+
+
+})
+
+export class Index implements OnInit {
+  ngOnInit() {
+    console.log('dupa')
+  }
+}
