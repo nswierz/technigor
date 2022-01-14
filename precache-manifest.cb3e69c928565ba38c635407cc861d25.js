6d00c3f55b9d417983926dc67f867acd81cdd59c
Natalia <nswierz.iwp@gmail.com>
2022-01-14 12:17:54 +0100

    january deployment

---
+++ precache-manifest.cb3e69c928565ba38c635407cc861d25.js
@@ -0,0 +1,542 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f789083b5ad6878edfeeab311be6a1d6",
+    "url": "/assets/bootstrap-4.0.0-dist/css/bootstrap-grid.css"
+  },
+  {
+    "revision": "8997ec71a78ea4b0da89935276f4188b",
+    "url": "/assets/bootstrap-4.0.0-dist/css/bootstrap-grid.min.css"
+  },
+  {
+    "revision": "f1f75a3daa995c078fda451f438d16fd",
+    "url": "/assets/bootstrap-4.0.0-dist/css/bootstrap-reboot.css"
+  },
+  {
+    "revision": "1acc02b102fc1f6d4117be1726fb367e",
+    "url": "/assets/bootstrap-4.0.0-dist/css/bootstrap-reboot.min.css"
+  },
+  {
+    "revision": "c17ae4f0529e0bc2fc887d2471ae124b",
+    "url": "/assets/bootstrap-4.0.0-dist/css/bootstrap.css"
+  },
+  {
+    "revision": "416bb9e03b223eba66e9a3ca5a9da02e",
+    "url": "/assets/bootstrap-4.0.0-dist/css/bootstrap.min.css"
+  },
+  {
+    "revision": "e52da38174949c713ccfb9ce929b0d4b",
+    "url": "/assets/bootstrap-4.0.0-dist/js/bootstrap.bundle.js"
+  },
+  {
+    "revision": "18a1ebc44d97e64b7461be8cdde9d766",
+    "url": "/assets/bootstrap-4.0.0-dist/js/bootstrap.bundle.min.js"
+  },
+  {
+    "revision": "bb38938b1b9056351722c813ddbbbe70",
+    "url": "/assets/bootstrap-4.0.0-dist/js/bootstrap.js"
+  },
+  {
+    "revision": "d061ab58270e692309b728296e90bba2",
+    "url": "/assets/bootstrap-4.0.0-dist/js/bootstrap.min.js"
+  },
+  {
+    "revision": "5c7b02f3302d6b581ca777ef107d7743",
+    "url": "/assets/css/bootstrap.min.css"
+  },
+  {
+    "revision": "a0e784c4ca94c271b0338dfb02055be6",
+    "url": "/assets/css/font-awesome.min.css"
+  },
+  {
+    "revision": "c03fe8704d90e35eba342d2ca2c5a530",
+    "url": "/assets/css/magnific-popup.css"
+  },
+  {
+    "revision": "59d83d1b3d639c45c70b4c34c534cbcb",
+    "url": "/assets/css/set1.css"
+  },
+  {
+    "revision": "26a36d3b280acea9c4742a08f0e02362",
+    "url": "/assets/css/simple-line-icons.css"
+  },
+  {
+    "revision": "f363d98a627833794dff535e0e13999e",
+    "url": "/assets/css/style.css"
+  },
+  {
+    "revision": "727195a3aad99a40ad0b0a87bf08921a",
+    "url": "/assets/css/swiper.min.css"
+  },
+  {
+    "revision": "e46160d067f40c91f0421b1a7a1a0274",
+    "url": "/assets/css/themify-icons.css"
+  },
+  {
+    "revision": "0d2717cd5d853e5c765ca032dfd41a4d",
+    "url": "/assets/fonts/FontAwesome.otf"
+  },
+  {
+    "revision": "f33df365d6d0255b586f2920355e94d7",
+    "url": "/assets/fonts/Simple-Line-Icons.eot"
+  },
+  {
+    "revision": "73a932562a1e314703469d0a352fcda9",
+    "url": "/assets/fonts/Simple-Line-Icons.svg"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/assets/fonts/Simple-Line-Icons.ttf"
+  },
+  {
+    "revision": "78f07e2c2a535c26ef21d95e41bd7175",
+    "url": "/assets/fonts/Simple-Line-Icons.woff"
+  },
+  {
+    "revision": "0cb0b9c589c0624c9c78dd3d83e946f6",
+    "url": "/assets/fonts/Simple-Line-Icons.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/assets/fonts/fontawesome-webfont.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/assets/fonts/fontawesome-webfont.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/assets/fonts/fontawesome-webfont.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/assets/fonts/fontawesome-webfont.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/assets/fonts/fontawesome-webfont.woff2"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/assets/fonts/slick.eot"
+  },
+  {
+    "revision": "526d7fdf63614222d376257221e8b754",
+    "url": "/assets/fonts/slick.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/assets/fonts/slick.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/assets/fonts/slick.woff"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/assets/fonts/themify.eot"
+  },
+  {
+    "revision": "cba68f986e60df8c74f4a53c3e39595c",
+    "url": "/assets/fonts/themify.svg"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/assets/fonts/themify.ttf"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/assets/fonts/themify.woff"
+  },
+  {
+    "revision": "1dbad7d31df4ed0095eff1ba02d33ffb",
+    "url": "/assets/images/1.jpg"
+  },
+  {
+    "revision": "d2ffdc71d259dd4393120c32dd892623",
+    "url": "/assets/images/IADC1.png"
+  },
+  {
+    "revision": "ab111904cbfada5703e9cf24b3a3f7d3",
+    "url": "/assets/images/IADC2.png"
+  },
+  {
+    "revision": "82d249d7200f4b52c099dd5fa6611d76",
+    "url": "/assets/images/IADC3.png"
+  },
+  {
+    "revision": "3c85656c4ae233a86f1f542bce72be77",
+    "url": "/assets/images/Iso_en.pdf"
+  },
+  {
+    "revision": "39d5a52ec43e46136657c161c1d52f49",
+    "url": "/assets/images/Iso_pl.pdf"
+  },
+  {
+    "revision": "a4b5940488e70940fe23ca6cb6b4ed41",
+    "url": "/assets/images/Technigor-catalog.pdf"
+  },
+  {
+    "revision": "00fef72184269753b954737eeb5723b8",
+    "url": "/assets/images/article-img2.jpg"
+  },
+  {
+    "revision": "b221575816fea8b51be9519148e88459",
+    "url": "/assets/images/article-img3.jpg"
+  },
+  {
+    "revision": "20b08f3c91a67fefc1b7144f9a6b7b4f",
+    "url": "/assets/images/bearings.jpg"
+  },
+  {
+    "revision": "28ee14961df945da2bae0b82b14072cf",
+    "url": "/assets/images/blog-thumb-1.jpg"
+  },
+  {
+    "revision": "0f6176acf91e6eb27ead270e4573593b",
+    "url": "/assets/images/certificate-en.jpg"
+  },
+  {
+    "revision": "065b2689b268e99d82f55958fb1730d4",
+    "url": "/assets/images/certificate-pl.jpg"
+  },
+  {
+    "revision": "dc0c0cf14366cd64f534831c88e9905c",
+    "url": "/assets/images/certificate.png"
+  },
+  {
+    "revision": "1f571c9c45f0e80fd01a3d7ec0ea9665",
+    "url": "/assets/images/cutting-structure.jpg"
+  },
+  {
+    "revision": "8d2412f8496a40d9f11363f78e04d272",
+    "url": "/assets/images/favicon.png"
+  },
+  {
+    "revision": "9e066156547d9ba541756a29b5dc1c16",
+    "url": "/assets/images/favicon.svg"
+  },
+  {
+    "revision": "129a335accdbfeadb92a01b092ca093a",
+    "url": "/assets/images/featured-img1.jpg"
+  },
+  {
+    "revision": "88470fef773e487568de085d7343f45f",
+    "url": "/assets/images/flushing.jpg"
+  },
+  {
+    "revision": "5bef74788c5c526d5ea894d327b16501",
+    "url": "/assets/images/footer-logo.png"
+  },
+  {
+    "revision": "7204c369b947e2509d66021266e7d96e",
+    "url": "/assets/images/gal2.jpg"
+  },
+  {
+    "revision": "e5b81be9d025ea4fa1698953f20ef2ae",
+    "url": "/assets/images/gal3.jpg"
+  },
+  {
+    "revision": "a0c0c6a967f4bf3c7ebc4190f4b284b1",
+    "url": "/assets/images/gal4.jpg"
+  },
+  {
+    "revision": "7ddde5b6e58e4a2249643cbbb69bec19",
+    "url": "/assets/images/gallery1.jpg"
+  },
+  {
+    "revision": "79a24e5415a46fa2500635b8850b9283",
+    "url": "/assets/images/gallery10.jpg"
+  },
+  {
+    "revision": "019d8b5485d00ec03dc108c1c31b4ac4",
+    "url": "/assets/images/gallery11.jpg"
+  },
+  {
+    "revision": "25a3922e0a935bbdb1fa8eeff1a0beb4",
+    "url": "/assets/images/gallery12.jpg"
+  },
+  {
+    "revision": "c132b0452e900201217e15282c4c9eeb",
+    "url": "/assets/images/gallery13.jpg"
+  },
+  {
+    "revision": "bd2b22740bb817939f13adc1222b8fc0",
+    "url": "/assets/images/gallery14.jpg"
+  },
+  {
+    "revision": "2138536c1721f0372c52bef7bc830f7d",
+    "url": "/assets/images/gallery15.jpg"
+  },
+  {
+    "revision": "c9ddef06cc5986ed3a41a7f2e4f38fc6",
+    "url": "/assets/images/gallery16.jpg"
+  },
+  {
+    "revision": "218ab54f6d7c3a6e9a0d945f16bc9f6a",
+    "url": "/assets/images/gallery17.jpg"
+  },
+  {
+    "revision": "bd7cb6a7b5cc5bae63d777fa0a254872",
+    "url": "/assets/images/gallery18.jpg"
+  },
+  {
+    "revision": "6dbd328cc3d3ee0e8160993467244746",
+    "url": "/assets/images/gallery19.jpg"
+  },
+  {
+    "revision": "6f8be9682d79afd072d39f9ef2e27961",
+    "url": "/assets/images/gallery2.jpg"
+  },
+  {
+    "revision": "5d05b1ace724dcb7424dda3ed4d87ff0",
+    "url": "/assets/images/gallery20.jpg"
+  },
+  {
+    "revision": "f3bcc9b4c7753ca9ef4c270dce91c38d",
+    "url": "/assets/images/gallery21.jpg"
+  },
+  {
+    "revision": "6fe15590385987732a43ac5e27aa4b4a",
+    "url": "/assets/images/gallery22.jpg"
+  },
+  {
+    "revision": "e3d06ce0ebc861b6ff09032b55ce8d20",
+    "url": "/assets/images/gallery3.jpg"
+  },
+  {
+    "revision": "80545dd54c7499fa91421f5019292233",
+    "url": "/assets/images/gallery4.jpg"
+  },
+  {
+    "revision": "3f9c97a8a0cb16c6c89a32ff3297faee",
+    "url": "/assets/images/gallery5.jpg"
+  },
+  {
+    "revision": "764ad9ffa29cc13ec9771d38aae6276c",
+    "url": "/assets/images/gallery6.jpg"
+  },
+  {
+    "revision": "bc27998b806f0248a6bbf08f61b362c8",
+    "url": "/assets/images/gallery7.jpg"
+  },
+  {
+    "revision": "dabe1b53e753b4c95cd736dda467d19d",
+    "url": "/assets/images/gallery8.jpg"
+  },
+  {
+    "revision": "7b62e860f67cb4a417537e59c32dfd58",
+    "url": "/assets/images/gallery9.jpg"
+  },
+  {
+    "revision": "616d7473e5dea7357f300fb3156f2a96",
+    "url": "/assets/images/galleryA.jpg"
+  },
+  {
+    "revision": "bbc2f539848c86cce8500fd476374d8c",
+    "url": "/assets/images/galleryB.jpg"
+  },
+  {
+    "revision": "bf1d1eaaef54f0bc882bc23e615989e4",
+    "url": "/assets/images/galleryC.jpg"
+  },
+  {
+    "revision": "6b5aa9a285c934f3f78f4496d45674c9",
+    "url": "/assets/images/galleryD.jpg"
+  },
+  {
+    "revision": "29c5d9f9135cd12c7b9e3fecf83bfa43",
+    "url": "/assets/images/galleryE.jpg"
+  },
+  {
+    "revision": "7335484e94d31bda8c67090cc16b8174",
+    "url": "/assets/images/galleryF.jpg"
+  },
+  {
+    "revision": "afa9a150cf332ac615dd01c8c8deda61",
+    "url": "/assets/images/geoterm.jpg"
+  },
+  {
+    "revision": "ea213adbed70e356eacff8a68d7cdac1",
+    "url": "/assets/images/headquarters-night.jpg"
+  },
+  {
+    "revision": "230741933669ad9c153c2e53486ed31f",
+    "url": "/assets/images/headquarters.jpg"
+  },
+  {
+    "revision": "51e0891f8e98688fee2b3d06f472b4a5",
+    "url": "/assets/images/land.jpg"
+  },
+  {
+    "revision": "530db42506490c9fe03c994afa114a84",
+    "url": "/assets/images/landing.jpg"
+  },
+  {
+    "revision": "3ac2fea0577acd0199979b5ba2622462",
+    "url": "/assets/images/landing.png"
+  },
+  {
+    "revision": "b54372226a49e784fba4f09039699992",
+    "url": "/assets/images/legs.jpg"
+  },
+  {
+    "revision": "886c73f741148a5b175bbb9064677c60",
+    "url": "/assets/images/lewy-obieg1.jpg"
+  },
+  {
+    "revision": "1c6aff5cf90a16ff7efe0cee73cc691f",
+    "url": "/assets/images/lewy-obieg2.jpg"
+  },
+  {
+    "revision": "b197ee38ac4b5e9fdb72a618832bb955",
+    "url": "/assets/images/lewy-obieg3.jpg"
+  },
+  {
+    "revision": "5e3ebe70b99d087db2c8953722f98db1",
+    "url": "/assets/images/lewy-obieg4.jpg"
+  },
+  {
+    "revision": "0e78b37bd7b6f805e77f39547ee648da",
+    "url": "/assets/images/lewyobieg.jpg"
+  },
+  {
+    "revision": "dce25099a03d2b2be84a3f1e68111b35",
+    "url": "/assets/images/logo.png"
+  },
+  {
+    "revision": "1ab855d66566368f80d63333e176772f",
+    "url": "/assets/images/logo1.png"
+  },
+  {
+    "revision": "071deea35bf3b965a86dbd5156f656ac",
+    "url": "/assets/images/map.jpg"
+  },
+  {
+    "revision": "eb6300b182357360a4640cac98c58516",
+    "url": "/assets/images/nieuszczelniony-productpage.jpg"
+  },
+  {
+    "revision": "c9743595c05f177eaf88a62a166278d7",
+    "url": "/assets/images/nieuszczelniony-productpage2.jpg"
+  },
+  {
+    "revision": "f4132d412669f931850f33a1d5c03227",
+    "url": "/assets/images/nieuszczelniony.jpg"
+  },
+  {
+    "revision": "fc1919afdb188afb213d3c012fdc5b90",
+    "url": "/assets/images/nomenclature.jpg"
+  },
+  {
+    "revision": "3eb783e90d0620998ad335ccac8087f9",
+    "url": "/assets/images/nomenclature1.jpg"
+  },
+  {
+    "revision": "78770e35166b7354f3e72a978a4740d6",
+    "url": "/assets/images/nomenclature2.jpg"
+  },
+  {
+    "revision": "982a18829201e5aad9f3006eeebcdb3c",
+    "url": "/assets/images/pompy-ciepla1.jpg"
+  },
+  {
+    "revision": "3bf587287e6c3d6a774aec293bbf320f",
+    "url": "/assets/images/pompy-ciepla2.jpg"
+  },
+  {
+    "revision": "d1af39ec57bedf9aebb7d0cd0c93401e",
+    "url": "/assets/images/pompy-ciepla3.jpg"
+  },
+  {
+    "revision": "8a9af11490178463d8937cbd94a26854",
+    "url": "/assets/images/pompy-ciepla4.jpg"
+  },
+  {
+    "revision": "830a79428a73f94f3f05ec88233e1ac9",
+    "url": "/assets/images/tuv.png"
+  },
+  {
+    "revision": "a430a6bbb82451f6351a56e1901c6eae",
+    "url": "/assets/images/tuv.svg"
+  },
+  {
+    "revision": "a2a9039f36ca186c51b3f27f0c906bd1",
+    "url": "/assets/images/uszczelniony.jpg"
+  },
+  {
+    "revision": "8feab7c467781baa344f50dda42a08e6",
+    "url": "/assets/images/uszczelniony1.jpg"
+  },
+  {
+    "revision": "261c2e5ad2896c43caf58e0759f0c2d5",
+    "url": "/assets/images/uszczelniony2.jpg"
+  },
+  {
+    "revision": "ce02a6a3a110b36db1525c7ca598e9c2",
+    "url": "/assets/images/uszczelniony3.jpg"
+  },
+  {
+    "revision": "5a8ab573978f2ce1398dfcc634c937ea",
+    "url": "/assets/images/uszczelniony4.jpg"
+  },
+  {
+    "revision": "4515dd60cb59a63832631bff288611ae",
+    "url": "/assets/images/zespolyrolkowe.jpg"
+  },
+  {
+    "revision": "65a6abc6166c4984daef8c340f8458eb",
+    "url": "/assets/images/zespolyrolkowe1.jpg"
+  },
+  {
+    "revision": "8480c0ffbcc1a782f638fc719dd20cc0",
+    "url": "/assets/images/zespolyrolkowe2.jpg"
+  },
+  {
+    "revision": "0d29bdc9a61db6a4d884788c28dfebab",
+    "url": "/assets/images/zespolyrolkowe3.jpg"
+  },
+  {
+    "revision": "62794b4d1e2269ba782bb45381ac1353",
+    "url": "/assets/images/zespolyrolkowe4.jpg"
+  },
+  {
+    "revision": "fb1817b96c65b6477cb55fedf53e86d9",
+    "url": "/assets/js/jquery-3.5.1.js"
+  },
+  {
+    "revision": "4e55c4139c2c74d4711784d9c5ded6b7",
+    "url": "/assets/js/script.js"
+  },
+  {
+    "revision": "00e71380428e4d416bed43b4cfebf857",
+    "url": "/assets/scss/style.scss"
+  },
+  {
+    "revision": "c38294631d940804093a",
+    "url": "/css/app.6637778f.css"
+  },
+  {
+    "revision": "6a586496dbdb37b4c278",
+    "url": "/css/chunk-vendors.f0ea0052.css"
+  },
+  {
+    "revision": "6c9c5d6cb33139bd1f9ccd8207d1655e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c38294631d940804093a",
+    "url": "/js/app.547dde15.js"
+  },
+  {
+    "revision": "6a586496dbdb37b4c278",
+    "url": "/js/chunk-vendors.7699206f.js"
+  },
+  {
+    "revision": "ee81eec23cde43b6645ad2b3e21c0371",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
